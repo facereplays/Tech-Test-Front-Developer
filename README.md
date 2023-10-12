dbf0c3949953e64fc106fe69f67c48c6e4b73912
facereplays <facereplays@gmail.com>
2023-10-12 16:31:19 +0200

    tests fixes

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
 # Aitaca Frontend Developer Test
 ## Context
-We are looking for a passionate Frontend Developer with outstanding skills to join our team. This test will evaluate your experience and aptitude in frontend development, especially in relation to our needs and objectives.
 
+Forked from https://github.com/aitacaremote/Tech-Test-Front-Developer
 ## General Instructions
-Fork this repository to your GitHub account. Complete the tasks described below. Once finished, create a Pull Request to this repository so that we can review your solution.
+
+Fork this repositiory, fetch,  run "npm install"  (minimal versions:)
+"node": "18.14.2",
+"npm": "8.19.2"
+), CD to directory and run "ng serve"
+
+
 
 ## Tasks: Responsive Interface
-Develop a responsive webpage using HTML, CSS, and Angular. This page should contain at least one form with validation, a couple of pages or three where routing is used, and some visual components (such as cards, buttons, among others).
-
-Add at least one unit test that tests some of the developed functionalities.
+Developed with Angular Material and Tailwind CSS.
+All containers excluding 3D ThreeJs canvas in lazy-loading example are responsive
 
 ### BONUS
 
-- Use of observables and some of their operators (map, filter, reduce…)
-- Use of Material
-- Ensure that your page is optimized for speed and scalability
-- Implement at least one security measure (such as protection against XSS or CSRF attacks).
-- (Optional): Access to the device’s camera and integration with some simple MediaPipe functionality (https://developers.google.com/mediapipe) on one of the pages
+#### Use of observables and some of their operators (map, filter, reduce…)
+- implemented in Form page at sanitizing input and countries autofill
+#### Use of Material
+- Implemented
+##### Ensure that your page is optimized for speed and scalability
+- Loading symbols, lazy-loading heavy modules
+#### Implement at least one security measure (such as protection against XSS or CSRF attacks).
+- Textarea at Form page is sanitized from possible code injection
 
-## GitHub Collaboration
-During development, create at least three commits to show how you manage version control and organize your work. Make sure your code is well-commented and organized.
+#### Access to the device’s camera and integration with some simple MediaPipe functionality (https://developers.google.com/mediapipe) on one of the pages
+- handlandmarks recognition implied at Video page
+#### GitHub Collaboration
+done
 
-## Documentation
-Update README.md to include instructions on how to run and test your webpage and any additional functionality you have implemented.
--- src/app/components/face-recognition/face-recognition.component.ts
+++ src/app/components/face-recognition/face-recognition.component.ts
@@ -14,11 +14,10 @@ import {DrawService} from "../../services/draw.service";
   templateUrl: './face-recognition.component.html',
   styleUrls: ['./face-recognition.component.css']
 })
-export class FaceRecognitionComponent implements OnInit, AfterViewInit{
-  videoHeight: string = "360px";
-  videoWidth: string = "480px";
+export class FaceRecognitionComponent implements OnInit, AfterViewInit {
+
   runningMode: "IMAGE" | "VIDEO" = "IMAGE";
-  resultWidthHeigth:number = 256;
+
   imageSegmenter?: ImageSegmenter;
   labels?: string[];
   canvasClick?: HTMLCanvasElement;
@@ -26,154 +25,18 @@ export class FaceRecognitionComponent implements OnInit, AfterViewInit{
   mode?: string;
   @ViewChild("myCanvas") myCanvas!: ElementRef;
   @ViewChild("myVideo") myVideo!: ElementRef;
-
-
- hasGetUserMedia = ()=> !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
   handLandmarkerResult?: HandLandmarkerResult;
   legendColors = environment.legendColors;
   lastWebcamTime = -1;
-public results: HandLandmarkerResult | undefined;
-
-  constructor(private drawService: DrawService){
-
-
-  }
-
-
-   createHandLandmarker = async () => {
-    const vision = await FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
-    );
-
-
-
-
- this.handLandmarker = await HandLandmarker.createFromOptions(vision, {
-      baseOptions: {
-        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
-        delegate: "GPU"
-      },
-      runningMode: "VIDEO",
-      numHands: 2
-    });
-
-
-
-   // demosSection.classList.remove("invisible");
-  };
-
-
-/*
-
-*/
-
-
- createImageSegmenter = async () => {
-   const audio = await FilesetResolver.forVisionTasks(
-     "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
-   );
-   this.imageSegmenter = await ImageSegmenter.createFromOptions(audio, {
-     baseOptions: {
-       modelAssetPath:
-         "https://storage.googleapis.com/mediapipe-models/image_segmenter/deeplab_v3/float32/1/deeplab_v3.tflite",
-       delegate: "GPU"
-     },
-     runningMode: this.runningMode,
-     outputCategoryMask: true,
-     outputConfidenceMasks: false
-   });
-   this.labels = this.imageSegmenter.getLabels();
- }
-  callback = (result: ImageSegmenterResult) => {
-    const cxt = this.canvasClick!.getContext("2d");
-    if(cxt &&  this.canvasClick)
-    {
-
-
-    // @ts-ignore
-      const {width, height} = result.categoryMask;
-    let imageData = cxt.getImageData(0, 0, width, height).data;
-      this.canvasClick.width = width;
-      this.canvasClick.height = height;
-    let category: String = "";
-    const mask = result.categoryMask!.getAsUint8Array() ;
-mask.forEach((maski,i)=>{
-      if (maski > 0) {
-        category = this.labels![maski];
-      }
-      const legendColor = this.legendColors[maski % this.legendColors.length]
-
-      imageData[i * 4] = (legendColor[0] + imageData[i * 4]) / 2;
-      imageData[i * 4 + 1] = (legendColor[1] + imageData[i * 4 + 1]) / 2;
-      imageData[i * 4 + 2] = (legendColor[2] + imageData[i * 4 + 2]) / 2;
-      imageData[i * 4 + 3] = (legendColor[3] + imageData[i * 4 + 3]) / 2;
-    })
-    const uint8Array = new Uint8ClampedArray(imageData.buffer);
-    const dataNew = new ImageData(uint8Array, width, height);
-    cxt.putImageData(dataNew, 0, 0);
-   /* const p: HTMLElement = event.target.parentNode.getElementsByClassName(
-      "classification"
-    )[0];
-    p.classList.remove("removed");
-    p.innerText = "Category: " + category;
-
-    */
-    }
-  }
-
-    predictWebcam =async () => {
-//console.log(this.video);
-    if (this.video.currentTime === this.lastWebcamTime) {
-      if (this.webcamRunning === true) {
-
-        window.requestAnimationFrame(this.predictWebcam);
-      }
-      return;
-    }
-    this.lastWebcamTime = this.video.currentTime;
-      /***
-       * filling canvas with video
-       * dull ing video with white
-       *
-       */
-    this.canvasCtx!.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-      this.canvasCtx!.fillStyle = "#ffffff99";
-      this.canvasCtx!.fillRect( 0, 0, this.video.videoWidth, this.video.videoHeight)
-      //this.canvasCtx!.drawRecta(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-    // Do not segmented if imageSegmenter hasn't loaded
-    if (this.imageSegmenter === undefined) {
-      //return;
-    }
-
-    // if image mode is initialized, create a new segmented with video runningMode
-
-      this.runningMode = "VIDEO";
-    let startTimeMs = performance.now();
-    //  console.log(this.lastWebcamTime,startTimeMs,this.runningMode);
-      /***
-       *
-       * getting hands landmarks
-       *
-       */
-     this.results = this.handLandmarker?.detectForVideo(this.video, startTimeMs);
-
-      if (this.results && this.results.landmarks) {
-
-        for (const landmarks of this.results.landmarks) {
-        //  console.log(JSON.stringify(landmarks));
-          this.drawService.drawConnectors(this.canvasCtx!, landmarks, environment.HAND_CONNECTIONS, {
-            color: "#00FF00",
-            lineWidth: 5
-          },[this.video.videoWidth,this.video.videoHeight]);
-          this.drawService.drawLandmarks(this.canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
-        }
-        if (this.webcamRunning === true ) {
-          window.requestAnimationFrame(this.predictWebcam);
-        }
-      }
-
-      if (this.imageSegmenter) this.imageSegmenter.segmentForVideo(this.video, startTimeMs, this.callbackForVideo);
-  }
+  public results: HandLandmarkerResult | undefined;
+  test: boolean = false;
+  public serving: boolean;
+  private canvasElement?: HTMLCanvasElement;
+  private canvasCtx?: CanvasRenderingContext2D | null;
+  // @ts-ignore
+  private video: HTMLVideoElement;
+  private handLandmarker?: HandLandmarker;
+  private faceLandmarker?: FaceLandmarker;
 
 
   /***
@@ -183,9 +46,174 @@ mask.forEach((maski,i)=>{
    * imagesegmenting
    * @param result
    */
+  /***
+   *
+   * hand servuce results
+   * @private
+   */
+  private handLandResults: HandLandmarkerResult | undefined;
+  private marks: any[] = [];
+
+  constructor(private drawService: DrawService) {
+    this.serving = false;
+
+  }
+
+  /***
+   *
+   * check media if available
+   */
+  hasGetUserMedia = () => !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+
+  createHandLandmarker = async () => {
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+    );
 
 
-   callbackForVideo = (result: ImageSegmenterResult) => {
+    this.handLandmarker = await HandLandmarker.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+        delegate: "GPU"
+      },
+      runningMode: "VIDEO",
+      numHands: 2
+    });
+
+
+    // demosSection.classList.remove("invisible");
+  };
+
+  /*
+  fpor image segmenter case
+
+
+  */
+  createImageSegmenter = async () => {
+    const audio = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
+    );
+    this.imageSegmenter = await ImageSegmenter.createFromOptions(audio, {
+      baseOptions: {
+        modelAssetPath:
+          "https://storage.googleapis.com/mediapipe-models/image_segmenter/deeplab_v3/float32/1/deeplab_v3.tflite",
+        delegate: "GPU"
+      },
+      runningMode: this.runningMode,
+      outputCategoryMask: true,
+      outputConfidenceMasks: false
+    });
+    this.labels = this.imageSegmenter.getLabels();
+  }
+
+  callback = (result: ImageSegmenterResult) => {
+    const cxt = this.canvasClick!.getContext("2d");
+    if (cxt && this.canvasClick) {
+
+
+      // @ts-ignore
+      const {width, height} = result.categoryMask;
+      let imageData = cxt.getImageData(0, 0, width, height).data;
+      this.canvasClick.width = width;
+      this.canvasClick.height = height;
+      let category: String = "";
+      const mask = result.categoryMask!.getAsUint8Array();
+      mask.forEach((maski, i) => {
+        if (maski > 0) {
+          category = this.labels![maski];
+        }
+        const legendColor = this.legendColors[maski % this.legendColors.length]
+
+        imageData[i * 4] = (legendColor[0] + imageData[i * 4]) / 2;
+        imageData[i * 4 + 1] = (legendColor[1] + imageData[i * 4 + 1]) / 2;
+        imageData[i * 4 + 2] = (legendColor[2] + imageData[i * 4 + 2]) / 2;
+        imageData[i * 4 + 3] = (legendColor[3] + imageData[i * 4 + 3]) / 2;
+      })
+      const uint8Array = new Uint8ClampedArray(imageData.buffer);
+      const dataNew = new ImageData(uint8Array, width, height);
+      cxt.putImageData(dataNew, 0, 0);
+      /* const p: HTMLElement = event.target.parentNode.getElementsByClassName(
+         "classification"
+       )[0];
+       p.classList.remove("removed");
+       p.innerText = "Category: " + category;
+
+       */
+    }
+  }
+
+  /***
+   * frame by frame service for reading
+   *
+   *
+   */
+  predictWebcam = async () => {
+
+    if (this.video.currentTime === this.lastWebcamTime || !this.serving) {
+      if (this.webcamRunning === true) {
+
+        window.requestAnimationFrame(this.predictWebcam);
+      }
+      return;
+    }
+    this.lastWebcamTime = this.video.currentTime;
+    /***
+     * filling canvas with video
+     * dull ing video with white
+     *
+     */
+    this.canvasCtx!.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+    this.canvasCtx!.fillStyle = "#ffffff99";
+    this.canvasCtx!.fillRect(0, 0, this.video.videoWidth, this.video.videoHeight)
+    //this.canvasCtx!.drawRecta(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+    // Do not segmented if imageSegmenter hasn't loaded
+    if (this.imageSegmenter === undefined) {
+      //return;
+    }
+
+    // if image mode is initialized, create a new segmented with video runningMode
+
+    this.runningMode = "VIDEO";
+    let startTimeMs = performance.now();
+    //  console.log(this.lastWebcamTime,startTimeMs,this.runningMode);
+    /***
+     *
+     * getting hands landmarks
+     *
+     */
+    this.results = this.handLandmarker?.detectForVideo(this.video, startTimeMs);
+
+    if (this.results && this.results.landmarks) {
+
+      for (const landmarks of this.results.landmarks) {
+        //  console.log(JSON.stringify(landmarks));
+        /***
+         *
+         * sending canvas and handmarks to service to draw
+         *
+         */
+        this.drawService.drawConnectors(this.canvasCtx!, landmarks, environment.HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5
+        }, [this.video.videoWidth, this.video.videoHeight]);
+
+      }
+      if (this.webcamRunning === true) {
+        window.requestAnimationFrame(this.predictWebcam);
+      }
+    }
+
+    if (this.imageSegmenter) this.imageSegmenter.segmentForVideo(this.video, startTimeMs, this.callbackForVideo);
+  }
+
+  /***
+   *
+   *
+   *
+   * imagfe segmenter case
+   * @param result
+   */
+  callbackForVideo = (result: ImageSegmenterResult) => {
     let imageData = this.canvasCtx!.getImageData(
       0,
       0,
@@ -206,7 +234,7 @@ mask.forEach((maski,i)=>{
     const uint8Array = new Uint8ClampedArray(imageData.buffer);
     const dataNew = new ImageData(
       uint8Array,
-     this.video.videoWidth,
+      this.video.videoWidth,
       this.video.videoHeight
     );
     this.canvasCtx!.putImageData(dataNew, 0, 0);
@@ -215,17 +243,17 @@ mask.forEach((maski,i)=>{
     }
   }
 
-enableCam =   async (event: any)=> {
+  enableCam = async (event: any) => {
     if (this.imageSegmenter === undefined) {
-     // return;
+      // return;
     }
-
-    if (this.webcamRunning === true) {
+    this.serving = true;
+    if (this.webcamRunning) {
       this.webcamRunning = false;
-    //  enableWebcamButton.innerText = "ENABLE SEGMENTATION";
+      //  enableWebcamButton.innerText = "ENABLE SEGMENTATION";
     } else {
       this.webcamRunning = true;
-     // enableWebcamButton.innerText = "DISABLE SEGMENTATION";
+      // enableWebcamButton.innerText = "DISABLE SEGMENTATION";
     }
 
     // getUsermedia parameters.
@@ -237,14 +265,8 @@ enableCam =   async (event: any)=> {
     this.video.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
     this.video.addEventListener("loadeddata", this.predictWebcam);
   }
-  private canvasElement?: HTMLCanvasElement;
-  private canvasCtx?: CanvasRenderingContext2D | null;
-  // @ts-ignore
-  private video: HTMLVideoElement;
-  private handLandmarker?: HandLandmarker;
-  private faceLandmarker?: FaceLandmarker;
 
-   createFaceLandmarker = async () =>{
+  createFaceLandmarker = async () => {
     const filesetResolver = await FilesetResolver.forVisionTasks(
       "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
     );
@@ -259,62 +281,55 @@ enableCam =   async (event: any)=> {
     });
 
   }
- // createFaceLandmarker();
-  private handLandResults: HandLandmarkerResult | undefined;
-private marks: any[] = [];
-test:boolean = false;
 
-   ngAfterViewInit()
-   {
-     this.canvasElement = this.myCanvas.nativeElement as HTMLCanvasElement;
+  ngAfterViewInit() {
+    this.canvasElement = this.myCanvas.nativeElement as HTMLCanvasElement;
     this.canvasCtx = this.canvasElement.getContext("2d");
 //this.test=true;
-     /***
-      *
-      * testing
-      *
-      */
-     if(this.test){
-     this.marks=JSON.parse('[{"x":0.4419788718223572,"y":0.9885884523391724,"z":-1.6559452831188537e-7},{"x":0.47772884368896484,"y":0.8524411916732788,"z":-0.02013668231666088},{"x":0.47463172674179077,"y":0.7098792195320129,"z":-0.030217472463846207},{"x":0.46790122985839844,"y":0.6086395382881165,"z":-0.0383816659450531},{"x":0.45867159962654114,"y":0.5593640804290771,"z":-0.04470393806695938},{"x":0.32611313462257385,"y":0.697811484336853,"z":-0.0019850439857691526},{"x":0.3783108592033386,"y":0.6746347546577454,"z":-0.021891487762331963},{"x":0.4129917323589325,"y":0.7202364206314087,"z":-0.04995590075850487},{"x":0.4171450138092041,"y":0.7597101926803589,"z":-0.07034149020910263},{"x":0.2923184931278229,"y":0.7551758289337158,"z":-0.0014537802198901772},{"x":0.3406887948513031,"y":0.7316475510597229,"z":-0.011543123982846737},{"x":0.37543851137161255,"y":0.7718098759651184,"z":-0.031710922718048096},{"x":0.3804970681667328,"y":0.8124983310699463,"z":-0.04908472299575806},{"x":0.27665817737579346,"y":0.8158678412437439,"z":-0.003018013434484601},{"x":0.30786821246147156,"y":0.7877399921417236,"z":-0.012737883254885674},{"x":0.341959148645401,"y":0.8199914693832397,"z":-0.01200360432267189},{"x":0.3522948622703552,"y":0.8536326289176941,"z":-0.012613429687917233},{"x":0.2702025771141052,"y":0.8762029409408569,"z":-0.006714301649481058},{"x":0.2892342805862427,"y":0.8445165753364563,"z":-0.010889739729464054},{"x":0.3187931776046753,"y":0.8701635599136353,"z":0.0017439138609915972},{"x":0.3319021463394165,"y":0.897994875907898,"z":0.01243297103792429}]');
+    /***
+     *
+     * testing
+     *
+     */
+    if (this.test) {
+      this.marks = JSON.parse('[{"x":0.4419788718223572,"y":0.9885884523391724,"z":-1.6559452831188537e-7},{"x":0.47772884368896484,"y":0.8524411916732788,"z":-0.02013668231666088},{"x":0.47463172674179077,"y":0.7098792195320129,"z":-0.030217472463846207},{"x":0.46790122985839844,"y":0.6086395382881165,"z":-0.0383816659450531},{"x":0.45867159962654114,"y":0.5593640804290771,"z":-0.04470393806695938},{"x":0.32611313462257385,"y":0.697811484336853,"z":-0.0019850439857691526},{"x":0.3783108592033386,"y":0.6746347546577454,"z":-0.021891487762331963},{"x":0.4129917323589325,"y":0.7202364206314087,"z":-0.04995590075850487},{"x":0.4171450138092041,"y":0.7597101926803589,"z":-0.07034149020910263},{"x":0.2923184931278229,"y":0.7551758289337158,"z":-0.0014537802198901772},{"x":0.3406887948513031,"y":0.7316475510597229,"z":-0.011543123982846737},{"x":0.37543851137161255,"y":0.7718098759651184,"z":-0.031710922718048096},{"x":0.3804970681667328,"y":0.8124983310699463,"z":-0.04908472299575806},{"x":0.27665817737579346,"y":0.8158678412437439,"z":-0.003018013434484601},{"x":0.30786821246147156,"y":0.7877399921417236,"z":-0.012737883254885674},{"x":0.341959148645401,"y":0.8199914693832397,"z":-0.01200360432267189},{"x":0.3522948622703552,"y":0.8536326289176941,"z":-0.012613429687917233},{"x":0.2702025771141052,"y":0.8762029409408569,"z":-0.006714301649481058},{"x":0.2892342805862427,"y":0.8445165753364563,"z":-0.010889739729464054},{"x":0.3187931776046753,"y":0.8701635599136353,"z":0.0017439138609915972},{"x":0.3319021463394165,"y":0.897994875907898,"z":0.01243297103792429}]');
 // Get DOM elements
 
-     this.drawService.drawConnectors(this.canvasCtx!, this.marks, environment.HAND_CONNECTIONS, {
-       color: "#00FF00",
-       lineWidth: 5
-     },[this.video.videoWidth,this.video.videoHeight]);
-   }    /***
-      *
-      * end testing
-      */
-     this.video = this.myVideo.nativeElement as HTMLVideoElement;
-   }
-   ngOnInit()
-   {
-/*
-
- */
-
-     //demosSection.classList.remove("invisible");
-
-  // this.createImageSegmenter();
-
-this.createHandLandmarker();
+      this.drawService.drawConnectors(this.canvasCtx!, this.marks, environment.HAND_CONNECTIONS, {
+        color: "#00FF00",
+        lineWidth: 5
+      }, [this.video.videoWidth, this.video.videoHeight]);
+    }
+    /***
+     *
+     * end testing
+     */
+    this.video = this.myVideo.nativeElement as HTMLVideoElement;
 
 
-     /**
-    * Demo 1: Segmented images on click and display results.
-    */
+    this.createHandLandmarker();
+  }
+
+  ngOnInit() {
+    /*
+
+     */
+
+    //demosSection.classList.remove("invisible");
+
+    // this.createImageSegmenter();
 
 
 
 
+    /**
+     * Demo 1: Segmented images on click and display results.
+     */
 
 
-
-
-   /********************************************************************
-    // Demo 2: Continuously grab image from webcam stream and segmented it.
-    ********************************************************************/
+    /********************************************************************
+     // Demo 2: Continuously grab image from webcam stream and segmented it.
+     ********************************************************************/
 
 // Check if webcam access is supported.
 
@@ -322,24 +337,22 @@ this.createHandLandmarker();
 // Get segmentation from the webcam
 
 
-
-
 // Enable the live webcam view and start imageSegmentation.
 
 
 // If webcam supported, add event listener to button.
-     /*
-   if (this.hasGetUserMedia()) {
-     enableWebcamButton = document.getElementById(
-       "webcamButton"
-     ) as HTMLButtonElement;
-     enableWebcamButton.addEventListener("click", enableCam);
-   } else {
-     console.warn("getUserMedia() is not supported by your browser");
-   }
+    /*
+  if (this.hasGetUserMedia()) {
+    enableWebcamButton = document.getElementById(
+      "webcamButton"
+    ) as HTMLButtonElement;
+    enableWebcamButton.addEventListener("click", enableCam);
+  } else {
+    console.warn("getUserMedia() is not supported by your browser");
+  }
 */
 
- }
+  }
 
 
 }
